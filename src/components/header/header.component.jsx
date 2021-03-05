11a7f7e6d5bb0b161e692b4a6d6a8e1448f348c8
teju <tejaswiniteju7799@gmail.com>
2021-03-05 12:42:44 +0530

    header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './header.styles.scss'
+
+import { Link } from 'react-router-dom'
+
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+export const Header = () => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options ">
+      <Link className="option" to="/shop">
+        shop
+      </Link>
+      <Link className="option" to="/shop">
+        contact
+      </Link>
+    </div>
+  </div>
+)
+
+export default Header
