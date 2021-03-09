ef02e087d39db803c41d8e985887d7457528e784
teju <tejaswiniteju7799@gmail.com>
2021-03-09 10:58:30 +0530

    redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
-
+import { connect } from 'react-redux'
 import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
@@ -32,4 +32,8 @@ const Header = ({ currentUser }) => (
   </div>
 )
 
-export default Header
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+})
+
+export default connect(mapStateToProps)(Header)
